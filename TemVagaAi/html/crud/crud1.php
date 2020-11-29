663aea336a43d77b033098a3bbb3553d2da9f9ad
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2020-11-29 15:57:03 -0300

    CRUD para cadastro de vagas feito.

--- TemVagaAi/html/crud/crud1.php
+++ TemVagaAi/html/crud/crud1.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro - Tem Vaga Ai!</title>
+    <link rel="stylesheet" href="../../css/IdentidadeVisual.css">
     <link rel="stylesheet" href="../../css/folhaDeEstiloCadastroCRUD.css">
     <script src="../../scripts/funcoes.js"></script>
     <script>
         function fnSubmit(){
+            /*
             var continuar = confirm("Clique em 'Ok', para confirmar as alterações.");
             if(continuar){
 
@@ -40,9 +42,14 @@
                 } else {
                     alert(erros);
                 }
+            }*/
+            var continuar = confirm("Clique em 'Ok', para confirmar as alterações.");
+            if(continuar){
+                document.getElementById('f1').submit();
             }
         }
         function fnExclude(id){
+            
             var continuar = confirm("Clique em 'Ok', para excluir o registro.");
             if(continuar){
                 document.getElementById('f1').submit();
@@ -56,116 +63,164 @@
 </head>
 <body>
 
-<h3 class="obrigatorio">*E-mail deve ser único, usuario e senha são obrigatórios. Telefone apenas números e no máximo 11 números.</h3>
-<form action="crud2.php" method="post" id="f1" name="f1">
-<?php
-    include('connect.php');
 
-    // Se existe GET id e GET mode
-    if(isset($_GET['id']) && isset($_GET['mode'])){
-        
-        if($_GET['id'] != '' && $_GET['mode'] == 'update'){
-            $id = $_GET['id'];
+<!-- div aviso do covid-19 -->
+<div id="aviso">
+    <div class="AvisoEsquerda" id="AvisoEsquerda">
+        COVID-2019: Seguimos todas as medidas de segurança à risca.
+        Saiba mais clicando
+        <a href="https://coronavirus.saude.gov.br/">aqui.</a>
+    </div>
+    <div class="AvisoDireita">
+        <button id="BotaoAvisoCovid" onclick="fecharAvisoCovid()">X</button>
+    </div>
+</div>
+<!-- div topo da pagina-->
+<div id="cabecalho">
+    <a href="../../index.html">
+        <img id="logo" src="../../imagens/LOGO TEM VAGA AI.png" alt="logo">
+    </a>
+    <form action="../../html/paginaresultados2.html" form="entrada">
+        <input type="text" id="pesuisa" name="pesquisa" placeholder="Buscar..." class="TextoPesquisar">
+    </form>
 
-            $sql = "SELECT * FROM Usuario WHERE id=$id";
-            $result = $conn->query($sql);
+    <form action="../../html/paginaresultados2.html" id="entrada">
+        <select id="Categorias" name="Categorias" form="entrada">
+            <option value="Chacaras">Chacaras</option>
+            <option value="Apartamentos">Apartamentos</option>
+            <option value="Ranchos Rios">Ranchos Rios</option>
+            <option value="Casas Beira Mar">Casas Beira Mar</option>
+        </select>
+        <select id="Cidades" name="Cidades" form="entrada">
+            <option value="Rio Paranaiba">Rio Paranaiba</option>
+            <option value="São Gotardo">São Gotardo</option>
+            <option value="Tres Marias">Tres Marias</option>
+            <option value="Rio de Janeiro">Rio de Janeiro</option>
+        </select>
+        <input type="submit">
+    </form>
 
-            if($result->num_rows > 0){
-                while($row = $result->fetch_assoc()){
-                    $id = $row['id'];
-                    $usuario = $row['usuario'];
-                    $senha = $row['senha'];
-                    $email = $row['email'];
-                    $nome = $row['nome'];
-                    $telefone = $row['telefone'];                    
-                }            
+    <ul id="BotoesMenu">
+        <li class="Menu"> <a class="Menu" href="http://localhost/TemVagaAiProjeto/TemVagaAi/html/crud/crud1.php">Cadastro</a> </li>
+        <li class="Menu"> <a class="Menu" href="#AvisosMensagens">Avisos e Mensagens</a> </li>
+        <li class="Menu"> <a class="Menu" href="#Login">Login</a> </li>
+    </ul>
+</div>
+
+<div id="formInserirExterna">
+    <div id="formInserirInterna">
+        <h3 class="obrigatorio">*E-mail deve ser único, usuario e senha são obrigatórios. Telefone apenas números e no máximo 11 números.</h3>
+        <form action="crud2.php" method="post" id="f1" name="f1">
+        <?php
+            include('connect.php');
+
+            // Se existe GET id e GET mode
+            if(isset($_GET['id']) && isset($_GET['mode'])){
+                
+                if($_GET['id'] != '' && $_GET['mode'] == 'update'){
+                    $id = $_GET['id'];
+
+                    $sql = "SELECT * FROM vaga WHERE id=$id";
+                    $result = $conn->query($sql);
+
+                    if($result->num_rows > 0){
+                        while($row = $result->fetch_assoc()){
+                            $id = $row['id'];
+                            $nome = $row['nome'];
+                            $descricao = $row['descricao'];
+                            $diaria = $row['diaria'];
+                            $caminhoFoto = $row['caminhoFoto'];
+                            $cidade = $row['cidade'];                    
+                        }            
+                    }
+
+                    echo "
+                    <table>            
+                        <tr>
+                            <td><b>Nome: </b></td>
+                            <td><input type='text' name='nome' id='nome' value='$nome'></td>
+                        </tr>
+                        <tr>
+                            <td><b>Descricao: </b></td>
+                            <td><input type='text' id='descricao' name='descricao' value='$descricao'></td>
+                        </tr>
+                        <tr>
+                            <td><b>Diaria: </b></td>
+                            <td><input type='text' name='diaria' id='diaria' value='$diaria'></td>
+                        </tr>
+                        <tr>
+                            <td><b>CaminhoFoto: </b></td>
+                            <td><input type='text' name='caminhoFoto' id='caminhoFoto' value='$caminhoFoto'></td>
+                        </tr>
+                        <tr>
+                            <td><b>Cidade: </b></td>
+                            <td><input type='text' name='cidade' id='cidade' value='$cidade'></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>                
+                            <td>
+                                <br>
+                                <input type='button' onclick='fnSubmit();' value='Enviar'>   
+                            </td>
+                        </tr>
+                    </table>
+
+                    <input type='hidden' id='id' name='id' value='$id'>
+                    <input type='hidden' id='mode' name='mode' value='update'>";
+
+                }
+                
+            }else{
+                
+                echo "  <table>
+                    <tr>
+                        <td><b>Nome: </b></td>
+                        <td><input type='text' name='nome' id='nome'></td>
+                    </tr>
+                    <tr>
+                        <td><b>Descricao: </b></td>
+                        <td><input type='text' id='descricao' name='descricao'></td>
+                    </tr>
+                    <tr>
+                        <td><b>Diaria: </b></td>
+                        <td><input type='text' name='diaria' id='diaria'></td>
+                    </tr>
+                    <tr>
+                        <td><b>CaminhoFoto: </b></td>
+                        <td><input type='text' id='caminhoFoto' name='caminhoFoto'></td>
+                    </tr>
+                    <tr>
+                        <td><b>Cidade: </b></td>
+                        <td><input type='text' name='cidade' id='cidade'></td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>                
+                        <td>
+                            <br>
+                            <input type='button' onclick='fnSubmit();' value='Enviar'>                    
+                        </td>
+                    </tr>
+                </table>
+                <input type='hidden' id='id' name='id' value='-1'>
+                <input type='hidden' id='mode' name='mode' value='insert'>";
             }
 
-            echo "
-            <table>            
-                <tr>
-                    <td><b>Email: </b></td>
-                    <td><input type='text' name='email' id='email' value='$email'></td>
-                </tr>
-                <tr>
-                    <td><b>Usuario: </b></td>
-                    <td><input type='text' id='usuario' name='usuario' value='$usuario'></td>
-                </tr>
-                <tr>
-                    <td><b>Senha: </b></td>
-                    <td><input type='text' name='senha' id='senha' value='$senha'></td>
-                </tr>
-                <tr>
-                    <td><b>Nome: </b></td>
-                    <td><input type='text' id='nome' name='nome' value='$nome'></td>
-                </tr>
-                <tr>
-                    <td><b>Telefone: </b></td>
-                    <td><input type='text' name='telefone' id='telefone' value='$telefone'></td>
-                </tr>
-                <tr>
-                    <td>&nbsp;</td>                
-                    <td>
-                        <br>
-                        <input type='button' onclick='fnSubmit();' value='Enviar'>   
-                    </td>
-                </tr>
-            </table>
-
-            <input type='hidden' id='id' name='id' value='$id'>
-            <input type='hidden' id='mode' name='mode' value='update'>";
-
-        }
-        
-    }else{
-        
-        echo "  <table>
-            <tr>
-                <td><b>Email: </b></td>
-                <td><input type='text' name='email' id='email'></td>
-            </tr>
-            <tr>
-                <td><b>Usuario: </b></td>
-                <td><input type='text' id='usuario' name='usuario'></td>
-            </tr>
-            <tr>
-                <td><b>Senha: </b></td>
-                <td><input type='text' name='senha' id='senha'></td>
-            </tr>
-            <tr>
-                <td><b>Nome: </b></td>
-                <td><input type='text' id='nome' name='nome'></td>
-            </tr>
-            <tr>
-                <td><b>Telefone: </b></td>
-                <td><input type='text' name='telefone' id='telefone'></td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>                
-                <td>
-                    <br>
-                    <input type='button' onclick='fnSubmit();' value='Enviar'>                    
-                </td>
-            </tr>
-        </table>
-        <input type='hidden' id='id' name='id' value='-1'>
-        <input type='hidden' id='mode' name='mode' value='insert'>";
-    }
-
-    $conn->close();
-?>
-</form>
+            $conn->close();
+        ?>
+        </form>
+    </div>
+</div>
 
 <hr>
 
 <table class="list">
     <tr class="list">
         <th class="list">id</th>
-        <th class="list">email</th>
-        <th class="list">usuario</th>
-        <th class="list">senha</th>
         <th class="list">nome</th>
-        <th class="list">telefone</th>
+        <th class="list">descricao</th>
+        <th class="list">diaria</th>
+        <th class="list">caminhoFoto</th>
+        <th class="list">cidade</th>
         <th>&nbsp;</th>
         <th>&nbsp;</th>
     </tr>
@@ -173,28 +228,28 @@
 <?php # Lista o select do BD
 
 include('connect.php');
-$sql = "SELECT * FROM Usuario";
+$sql = "SELECT * FROM vaga";
 $result = $conn->query($sql);
 
 if($result->num_rows > 0){
     // Coloca na tabela cada linha da tabela do BD
     while($row = $result->fetch_assoc()){
         $id = $row['id'];
-        $email = $row['email'];
-        $usuario = $row['usuario'];
         $nome = $row['nome'];
-        $senha = $row['senha'];
-        $telefone = $row['telefone'];
+        $descricao = $row['descricao'];
+        $diaria = $row['diaria'];
+        $caminhoFoto = $row['caminhoFoto'];
+        $cidade = $row['cidade'];    
         
 
         echo "
         <tr class='list'>
             <td class='list'>$id</td>
-            <td class='list'>$email</td>
-            <td class='list'>$usuario</td>
-            <td class='list'>$senha</td>
             <td class='list'>$nome</td>
-            <td class='list'>$telefone</td>
+            <td class='list'>$descricao</td>
+            <td class='list'>$diaria</td>
+            <td class='list'>$caminhoFoto</td>
+            <td class='list'>$cidade</td>
             
             
             <td class='action'>
@@ -226,9 +281,5 @@ $conn->close();
 
 
 
-
-
-
-    
 </body>
 </html>
--- TemVagaAi/html/crud/crud2.php
+++ TemVagaAi/html/crud/crud2.php
@@ -6,69 +6,49 @@ function test_input($data){
     echo "to aqui!";
     return $data;
 }
-function emailExistente($email){
-    include('connect.php');
-    $sql = "SELECT COUNT(*) FROM Usuario WHERE email=$email";
-    $result = $conn->query($sql);
-    if($result > 0){
-        echo "<script type='javascript'>alert('Email já cadastrado!');</script>";
-        return true;
-    }
-    return false;
-}
-function emailValido($email){
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
 function usuarioSenhaValidos($usuario, $senha){
     if(empty($usuario) || empty($senha)) {
         return false;
     }
     return true;
 }
-function telefoneValido($telefone){
-    if( is_int((int)$telefone)) {
-        return true;
-    }
-    return false;
-}
 
 include('connect.php');
 
 if($_GET['mode'] == 'exclude'){
     $id = $_GET['id'];    
-    $sql = "DELETE FROM Usuario WHERE id=$id";
+    $sql = "DELETE FROM vaga WHERE id=$id";
     $conn->query($sql);
     $conn->close();
 
 }else if($_POST['mode'] == 'insert'){
-    $usuario = test_input($_POST['usuario']);
-    $senha = test_input($_POST['senha']);
-    $email = test_input($_POST['email']);
-    if(emailExistente($email) == false && emailValido($email) && usuarioSenhaValidos($usuario, $senha) && telefoneValido($telefone)){
-        $nome = test_input($_POST['nome']);
-        $telefone = test_input($_POST['telefone']);
-
-        $sql = "INSERT INTO Usuario (usuario, senha, email, nome, telefone)
-                VALUES ('$usuario', '$senha', '$email', '$nome', '$telefone')";
-        $conn->query($sql);
-        $conn->close();
-    }    
+    
+    $nome = test_input($_POST['nome']);
+    $descricao = test_input($_POST['descricao']);
+    $diaria = test_input($_POST['diaria']);
+    $caminhoFoto = test_input($_POST['caminhoFoto']);
+    $cidade = test_input($_POST['cidade']);
+          
+    $sql = "INSERT INTO vaga (nome, descricao, diaria, caminhoFoto, cidade)
+            VALUES ('$nome', '$descricao', '$diaria', '$caminhoFoto', '$cidade')";
+    $conn->query($sql);
+    $conn->close();
+      
 
 }else if($_POST['mode'] == 'update'){
 
     $id = $_POST['id'];
-    $usuario = test_input($_POST['usuario']);
-    $senha = test_input($_POST['senha']);
-    $email = test_input($_POST['email']);
-    if(emailExistente($email) == false && emailValido($email) && usuarioSenhaValidos($usuario, $senha) && telefoneValido($telefone)){
-        $nome = test_input($_POST['nome']);
-        $telefone = test_input($_POST['telefone']);
-
-        $sql = "UPDATE Usuario SET usuario='$usuario', senha='$senha', email='$email',
-                        nome='$nome', telefone='$telefone' WHERE id='$id'";
-        $conn->query($sql);
-        $conn->close();
-    }
+    $nome = test_input($_POST['nome']);
+    $descricao = test_input($_POST['descricao']);
+    $diaria = test_input($_POST['diaria']);
+    $caminhoFoto = test_input($_POST['caminhoFoto']);
+    $cidade = test_input($_POST['cidade']);
+    
+    $sql = "UPDATE vaga SET nome='$nome', descricao='$descricao', diaria='$diaria',
+                    caminhoFoto='$caminhoFoto', cidade='$cidade' WHERE id='$id'";
+    $conn->query($sql);
+    $conn->close();
+    
 }
 
 header("Location: crud1.php");
